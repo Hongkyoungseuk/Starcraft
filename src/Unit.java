934ccef33c1075b86b86f4bd67438eff72420202
{Hongkyoungseuk} <tug234@daum.net>
2023-09-27 15:07:08 +0900

    0927.15:07

--- src/Unit.java
+++ src/Unit.java
@@ -4,9 +4,19 @@ public class Unit {
     private boolean fly;
 
 
+    public int attackPoint(Unit u) {    // u 에게 공격시 들어가는 데미지
+        if (u.getFly()) {
+            return 0;
+        } else {
+            return attack;
+        }
+
+    }
     public void damage(int n) {
         defense = defense - n;
     }
+
+    // TODO 여기다 method로 양쪽 둘다 대미지를 계산할수 있지 않을까???
 
     //get
     public int getAttack() {
